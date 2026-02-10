bd7c794c4fc72381d279791f34d748983b129a91
szroly <sep.roland@gmail.com>
2026-02-10 18:27:19 +0100

    planet/launches models

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -1,43 +1,48 @@
-const launches = new Map()
+// const launches = require("./launches.mongo");
+
+const launches = new Map();
 
 let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
-  mission: 'Kepler exploration X',
+  mission: "Kepler exploration X",
   rocket: "Explore IS1",
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
   upcoming: true,
-  success: true
-}
+  success: true,
+};
 
-launches.set(launch.flightNumber, launch)
+launches.set(launch.flightNumber, launch);
 
 function existLaunchWithId(launchId) {
-  return launches.has(launchId)
+  return launches.has(launchId);
 }
 
 function getAllLaunches() {
-  return Array.from(launches.values())
+  return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
   latestFlightNumber++;
-  launches.set(latestFlightNumber, Object.assign(launch, {
-    success: true,
-    upcoming: true,
-    customers: ['Zero to mastery', 'NASA'],
-    flightNumber: latestFlightNumber
-  }));
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["Zero to mastery", "NASA"],
+      flightNumber: latestFlightNumber,
+    }),
+  );
 }
 
 function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId)
-  aborted.upcoming = false
-  aborted.success = false
-  return aborted
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
 
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  // await mongoose.connect(MONGO_URL, {
+  //   useNewUrlParser: true,
+  //   useFindAndModify: false,
+  //   useCreateIndex: true,
+  //   useUnifiedTopology: true,
+  // });
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);

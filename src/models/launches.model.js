62e288f77fe11504a8cf6fae256016166ca2fd70
unknown <sep.roland@gmail.com>
2026-02-20 14:34:18 +0100

    implement pagination

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -5,18 +5,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: 'Kepler exploration X', //name
-  rocket: 'Explore IS1', //rocket.name
-  launchDate: new Date('December 27, 2030'), //date_local
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'], //payload.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
@@ -103,8 +91,13 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  
+  return await launchesDatabase
+  .find({}, { _id: 0, __v: 0 })
+  .sort({ flightNumber: 1 })
+  .skip(skip)
+  .limit(limit);
 }
 
 async function saveLaunch(launch) {

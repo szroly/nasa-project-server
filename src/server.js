faabe75baabed13b673d59aa9f0ae1499aa4eb65
szroly <sep.roland@gmail.com>
2026-02-21 18:24:13 +0100

    install dotenv

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 const http = require("http");
-
+require("dotenv").config();
 const app = require("./app");
-
-
 
 const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetsData } = require("./models/planets.model");
-const { loadLaunchesData } = require('./models/launches.model')
-
-
+const { loadLaunchesData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
@@ -21,8 +17,6 @@ async function startServer() {
   await loadLaunchesData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
-   
-    
   });
 }
 
